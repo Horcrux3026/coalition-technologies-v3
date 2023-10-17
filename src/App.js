4c57f26c0f32a6602fbe09eb55b603c67afa0ba2
unknown <laddharohan6@gmail.com>
2023-10-17 21:28:42 +0530

    climb

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./components/Header.js"
 import Stickynav from "./components/Stickynav.js"
 import History from "./components/History.js"
+import Climb from "./components/Climb"
 function App() {
   return(
     <div>
       <Header />
       <Stickynav />
       <History />
+      <Climb />
     </div>
   );
 }
---
+++ src/components/Climb.js
@@ -0,0 +1,19 @@
+import '../styles/climb.css';
+
+function climb() {
+  return (
+    <div>
+      <div id="climbText">
+        <div className="sectionNumber2">02.</div>
+        <div className="sectionTitle2">CLIMB</div>
+        <div className="sectionText2">
+          Cras scelerisque id quam sed dignissim Pellentesque urna nunc, gravida
+          quis hendrerit ac, tristique ut quam. Vivamus suscipit dignissim
+          tortor nec congue.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default climb;
